91017cf27cbd44bc4d750773af147e53d9127ec8
AndrewValenzuela20 <andyvalenzuela864@gmail.com>
2023-03-28 20:03:36 -0500

    resolve sun

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         solar.sun="1";
         solar.moon="2";
         solar.planets="3";
+
         featureSun Xron = new featureSun();
         Xron.temperature="123123123k";
         Xron.radius="1312313133";
